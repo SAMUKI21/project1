974d098e39edbb961773c337e3df1cbb14cb77a0
SAMUKI21 <107455429+SAMUKI21@users.noreply.github.com>
2023-06-12 21:17:49 +0530

    first page ui design first part

--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,3 @@
-
-
 import 'package:flutter/material.dart';
 
 class Homescreen extends StatelessWidget {
@@ -7,6 +5,40 @@ class Homescreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Center(
+          child: Column(
+        children:  [
+         const Text(
+            'welcome to Autoparadize.lk',
+            textAlign: TextAlign.center,
+          ),
+          const Text(
+            'save Time and Hassle with online car shopping',
+            textAlign: TextAlign.justify,
+            style: TextStyle(
+              color: Colors.green,
+            ),
+          ),
+           const Text(
+              'Autoparadize.lk lets you browse through thousands of cars from the comfort of your home. No need to visit multiple dealearships',
+              textAlign: TextAlign.center),
+           const Text('Already a member'),
+          TextButton(
+            onPressed: () {},
+            style: TextButton.styleFrom(
+              foregroundColor: Colors.black,
+              backgroundColor:Colors.white 
+
+            ),
+            child: const Text(
+              'TextButton (New)',
+              style: TextStyle(fontSize: 30),
+            ),
+          )
+    
+        ],
+      )),
+    );
   }
 }
