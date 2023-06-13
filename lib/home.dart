b2da989e9b0a5f88aa356c3a1538b18d1fd7c0ba
SAMUKI21 <107455429+SAMUKI21@users.noreply.github.com>
2023-06-13 11:30:03 +0530

    new changes adding container andimage

--- lib/home.dart
+++ lib/home.dart
@@ -7,38 +7,76 @@ class Homescreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-          child: Column(
-        children:  [
-         const Text(
+          child: Column(children: [
+        const Padding(
+          padding: EdgeInsets.only(left: 20, right: 20, top: 100, bottom: 10),
+          child: Text(
             'welcome to Autoparadize.lk',
             textAlign: TextAlign.center,
           ),
-          const Text(
+        ),
+        const Padding(
+          padding: EdgeInsets.only(left: 10, right: 10, top: 2, bottom: 2),
+          child: Text(
             'save Time and Hassle with online car shopping',
             textAlign: TextAlign.justify,
             style: TextStyle(
               color: Colors.green,
             ),
           ),
-           const Text(
-              'Autoparadize.lk lets you browse through thousands of cars from the comfort of your home. No need to visit multiple dealearships',
-              textAlign: TextAlign.center),
-           const Text('Already a member'),
-          TextButton(
-            onPressed: () {},
-            style: TextButton.styleFrom(
-              foregroundColor: Colors.black,
-              backgroundColor:Colors.white 
-
+        ),
+        const Padding(
+          padding: EdgeInsets.only(left: 16, right: 16, top: 50, bottom: 30),
+          child: Text(
+            'Autoparadize.lk lets you browse through thousands of cars from the comfort of your home. No need to visit multiple dealearships',
+            textAlign: TextAlign.center,
+          ),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              width: 30,
+              height: 4,
+              color: Colors.red,
             ),
-            child: const Text(
-              'TextButton (New)',
-              style: TextStyle(fontSize: 30),
+            const SizedBox(width: 10), // Add a small space of 10 units
+            Container(
+              width: 30,
+              height: 4,
+              color: Colors.grey,
             ),
-          )
-    
-        ],
-      )),
+            const SizedBox(width: 10), // Add a small space of 10 units
+            Container(
+              width: 30,
+              height: 4,
+              color: Colors.grey,
+            ),
+          ],
+        ),
+         const Padding(
+          padding: EdgeInsets.only(left: 16, right: 16, top: 50, bottom: 5),
+        child: Text('Already a member?'),
+         ),
+        TextButton(
+          onPressed: () {},
+          style: TextButton.styleFrom(
+            side: const BorderSide(width: 3.0),
+            foregroundColor: Colors.black,
+            backgroundColor: Colors.white,
+          ),
+          child: const Text(
+            'sign in',
+            style: TextStyle(fontSize: 24),
+          ),
+        ),
+        const SizedBox(height: 20), // Add space between the "Sign in" button and the image
+            Image.asset(
+              'asset/img1.jpg', // Replace with the path to your image file
+              width: 400,
+              height: 300,
+            ),
+      ])),
     );
   }
 }
